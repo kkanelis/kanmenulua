fdf545a0f77708e525835dac50d43e42a8627b21
Kanelis <kanelisirbulcina@gmail.com>
2025-02-01 19:01:26 +0200

    0.5.7 Protection / 01.02.2025

--- kan.lua
+++ kan.lua
@@ -1,5 +1,5 @@
 script_name("kanmenu")
-script_version("0.5.3 Protection / 01.02.2025")
+script_version("0.5.6 Protection / 01.02.2025")
 
 require "lib.moonloader"
 local event = require "lib.samp.events"
